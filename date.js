8627b6b1f98de7de5a59416df13eabfc5708dc1e
permingeat <laetitia.permingeat@le-campus-numerique.fr>
2022-04-19 10:09:15 +0200

    Gestionnaire de dépendances NPM

---
+++ date.js
@@ -0,0 +1,6 @@
+ let date=new Date();
+ //date.toLocaleDateString("fr")
+ //console.log(date.getDate());
+ console.log("Date du jour",date);
+ //console.log(date.getDay()+""+date.getMonth());
+ console.log("Numéro du jour : "+date.getDate() + " Numéro du mois : "+date.getMonth() + " année : " + date.getFullYear());
